ce39bcdd3448885d5ce3a18ce64610849a3e2cee
John Odom <joodom91@gmail.com>
2019-07-15 01:43:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(pigeon_data)
   pigeon_data.map do |metadata_color_gender_lives, intricate_variable_info|
     intricate_variable_info.map do |intricate_variable, pigeon_names|
       pigeon_names.map do |name|
-        if array_of_pigeons.include?(name)
+        if !array_of_pigeons.include?(name)
         array_of_pigeons << name
       end
       end
